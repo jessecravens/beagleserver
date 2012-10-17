9f723977dc1c01e62915b76de2a47db72ef8088c
Jesse Cravens <jesse.cravens@frogdesign.com>
2012-10-17 15:01:08 -0500

    initial twitter-bootstrap setup, added html5 multi file upload, node, mongo

--- app.js
+++ app.js
@@ -4,9 +4,18 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , mongoose = require('mongoose')
+  , fs = require('fs');
 
-var app = module.exports = express.createServer();
+var db = mongoose.createConnection('localhost', 'html5-tests');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('yo');
+});
+
+var app = express.createServer();
 
 // HTML Configuration
 	// app.configure(function(){
@@ -37,9 +46,8 @@ var app = module.exports = express.createServer();
 	app.configure(function(){
 	  app.set('views', __dirname + '/views');
 	  app.set('view engine', 'jade');
-	  app.use(express.bodyParser());
+	  app.use(express.bodyParser({uploadDir:'./uploads'}));
 	  app.use(express.methodOverride());
-	  // app.use(require('stylus').middleware({ src: __dirname + '/public' }));
 	  app.use(app.router);
 	  app.use(express.static(__dirname + '/public'));
 	});
@@ -55,19 +63,64 @@ var app = module.exports = express.createServer();
 // Jade Routes ////////////////////////////////////////////////////////////////////////
 	
 	app.get('/', routes.index);
-	
-	// app.get('/', function(req, res){
-	//   res.render("index.jade");
-	// });
-	
     app.get('/example1', routes.example1);
-	
-	// app.get('/example1', function(req, res){
-	//   res.render("example1.jade");
-	// });
-	
 	app.get('/example2', routes.example2);
 	app.get('/example3', routes.example3);
+
+// Img upload ////////////////////////////////////////////////////////////////////////
+
+	// app.post('/', function(req, res, next){
+
+	//   // connect-form adds the req.form object
+	//   // we can (optionally) define onComplete, passing
+	//   // the exception (if any) fields parsed, and files parsed
+	//   req.form.complete(function(err, fields, files){
+	//     if (err) {
+	//       next(err);
+	//     } else {
+	//       console.log('\nuploaded %s to %s'
+	//         ,  files.image.filename
+	//         , files.image.path);
+	//       res.redirect('back');
+	//     }
+	//   });
+
+	//   // We can add listeners for several form
+	//   // events such as "progress"
+	//   req.form.on('progress', function(bytesReceived, bytesExpected){
+	//     var percent = (bytesReceived / bytesExpected * 100) | 0;
+	//     process.stdout.write('Uploading: %' + percent + '\r');
+	//   });
+	// });
+
+	app.post('/file-upload', function(req, res, next) {
+	    console.log(req.body);
+	    console.log(req.files.file.path);
+	    res.send('File uploaded at: '  + req.files.file.size + ' bytes');
+	});
+
+	// app.post('/file-upload', function(req, res) {
+	//     // get the temporary location of the file
+	//     var tmp_path = req.files.file.path;
+	//     // set where the file should actually exists - in this case it is in the "images" directory
+	//     var target_path = './public/images/' + req.files.file.name;
+
+	//     console.log(fs);
+	//     // move the file from the temporary location to the intended location
+	//     fs.rename(tmp_path, target_path, function(err) {
+	//         if (err) throw err;
+	//         // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+	//         fs.unlink(tmp_path, function() {
+	//             if (err) throw err;
+	//             res.send('File uploaded to: ' + target_path + ' - ' + req.files.file.size + ' bytes');
+	//         });
+	//     });
+	// });
+
+
+// API server ////////////////////////////////////////////////////////////////////////
+
+
 
 
 // HTML Routes
@@ -99,5 +152,5 @@ var app = module.exports = express.createServer();
 	//   res.render("examples.html");
 	// });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3333);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
