bb35055cbe8b41a0141e333cc869181dcab9b795
Jesse Cravens <jesse.cravens@frogdesign.com>
2012-10-21 22:16:15 -0500

    added generic jasmine tests

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-
 /**
  * Module dependencies.
  */
-
 var express = require('express')
   , routes = require('./routes')
+  , connect = require('connect')
   , mongoose = require('mongoose')
-  , fs = require('fs');
+  , fs = require('fs')
+  , io = require('socket.io')
+  , useragent = require('useragent')
+  , gm = require('googlemaps');
 
 var db = mongoose.createConnection('localhost', 'html5-tests');
 
@@ -15,7 +17,7 @@ db.once('open', function () {
   console.log('yo');
 });
 
-
+// May want to use a Nested Schema?
 // var imageSchema = new mongoose.Schema({
 //     obj: { data: Buffer, contentType: String }
 // })
@@ -29,7 +31,19 @@ var imageSchema = new mongoose.Schema({
 // Image model
 var Image = db.model('Image', imageSchema);
 
+// Clients is a list of users who have connected
+var clients = [];
+var currentClient;
+
+///////////////////////////////////////////////////////////////////// SEND() UTILITY
+function send(message) { 
+  clients.forEach(function(client) {
+      client.send(message);
+  });
+}
+
 var app = express.createServer();
+var store  = new express.session.MemoryStore;
 
 // HTML Configuration
 	// app.configure(function(){
@@ -62,6 +76,12 @@ var app = express.createServer();
 	  app.set('view engine', 'jade');
 	  app.use(express.bodyParser({uploadDir:'./uploads'}));
 	  app.use(express.methodOverride());
+	  app.use(express.cookieParser());
+	  app.use(express.session({ secret: 'something', store: store }));
+	  // app.use(function (req, res) {
+	  //   res.end('<h2>Hello, your session id is ' + req.sessionID + '</h2>');
+	  // });
+
 	  app.use(app.router);
 	  app.use(express.static(__dirname + '/public'));
 	});
@@ -82,30 +102,6 @@ var app = express.createServer();
 	app.get('/example3', routes.example3);
 
 // Img upload ////////////////////////////////////////////////////////////////////////
-
-	// app.post('/', function(req, res, next){
-
-	//   // connect-form adds the req.form object
-	//   // we can (optionally) define onComplete, passing
-	//   // the exception (if any) fields parsed, and files parsed
-	//   req.form.complete(function(err, fields, files){
-	//     if (err) {
-	//       next(err);
-	//     } else {
-	//       console.log('\nuploaded %s to %s'
-	//         ,  files.image.filename
-	//         , files.image.path);
-	//       res.redirect('back');
-	//     }
-	//   });
-
-	//   // We can add listeners for several form
-	//   // events such as "progress"
-	//   req.form.on('progress', function(bytesReceived, bytesExpected){
-	//     var percent = (bytesReceived / bytesExpected * 100) | 0;
-	//     process.stdout.write('Uploading: %' + percent + '\r');
-	//   });
-	// });
 
 	app.post('/file-upload', function(req, res, next) {
 	    console.log(req.body);
@@ -133,25 +129,6 @@ var app = express.createServer();
 		});
 
 	});
-
-	// app.post('/file-upload', function(req, res) {
-	//     // get the temporary location of the file
-	//     var tmp_path = req.files.file.path;
-	//     // set where the file should actually exists - in this case it is in the "images" directory
-	//     var target_path = './public/images/' + req.files.file.name;
-
-	//     console.log(fs);
-	//     // move the file from the temporary location to the intended location
-	//     fs.rename(tmp_path, target_path, function(err) {
-	//         if (err) throw err;
-	//         // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-	//         fs.unlink(tmp_path, function() {
-	//             if (err) throw err;
-	//             res.send('File uploaded to: ' + target_path + ' - ' + req.files.file.size + ' bytes');
-	//         });
-	//     });
-	// });
-
 
 // API server ////////////////////////////////////////////////////////////////////////
 
@@ -186,3 +163,121 @@ var app = express.createServer();
 
 app.listen(process.env.PORT || 3333);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+///////////////////////////////////////////////////////////////////// SOCKET.IO SERVER
+// var sio = io.listen(app, {"heartbeats": false, "transports": ['websocket'], "close timeout": 100});
+// var sio = io.listen(app, {"heartbeats": false, "close timeout": 100});
+var sio = io.listen(app);
+//console.log(sio.settings);
+
+// var parseCookie = require('connect').utils.cookieParser;
+// console.log("parseCookie: " , parseCookie);
+
+//var parseCookie = require('connect').cookieParser;
+
+// var parseCookie = require('connect').utils.parseCookie;
+
+// // var conn = require('connect');
+// // console.log("conn: " , conn)
+// // console.log("parseCookie: " , parseCookie); 
+
+// sio.set('authorization', function (data, accept) {
+//     // check if there's a cookie header
+//     if (data.headers.cookie) {
+
+//     	console.log("data.headers.cookie " , data.headers.cookie);
+    	
+//         // if there is, parse the cookie
+//         // console.log("parseCookie: " , parseCookie)
+//         //data.cookie = parseCookie('secret-phrase');	
+        
+        
+//         data.cookie = parseCookie(data.headers.cookie);
+//         console.log("data.cookie : " + data.cookie);
+//         // note that you will need to use the same key to grad the
+//         // session id, as you specified in the Express setup.
+//         data.sessionID = data.cookie['express.sid'];
+
+//         console.log("data.sessionID: " , data.sessionID);
+
+//     } else {
+//        // if there isn't, turn down the connection with a message
+//        // and leave the function.
+//        return accept('No cookie transmitted.', false);
+//     }
+//     // accept the incoming connection
+//     accept(null, true);
+// });
+
+
+
+///////////////////////////////////////////////////////////////////// SOCKET.IO WEB SOCKETS
+sio.sockets.on('connection', function(client) {
+  // For each connection made add the client to the array of clients.
+  console.log('server connection EVENT FIRED');
+  console.log('CLIENT connected'); 
+  console.log('CLIENT ID: ' + client.id + ', TRANSPORT MECHANISM: ' + sio.transports[client.id].name);
+
+///////////////////////////////////////////////////////////////////// BUILD CLIENTS LIST
+  clients.push(client);
+  console.log(clients.length);
+
+  send(JSON.stringify({"clients": clients.length}));
+
+  // log each clients id
+  clients.forEach(function(client) {
+    // console.log('CLIENT ID: ' + client.id);
+    // console.log(client);
+  });
+
+
+  // We declare that the first message contains the SID.
+  // This is where we handle the first message.
+  client.once('message', function(sid) {
+
+  	console.log("getting a session on the first message")
+    store.get(sid, function(err, session) {
+      if (err || !session) {
+        // Do some error handling, bail.
+        return;
+      }
+
+      // Any messages following are your chat messages.
+      // client.on('message', function(message) {
+      //   if (message.email === session.email) {
+      //     socket.broadcast(message.text);
+      //   }
+      // });
+
+
+	  client.on('disconnect', function () {
+	 	console.log('disconnect EVENT FIRED');
+		// console.log(clients.length)
+		var index = clients.indexOf(client.id);
+		// console.log(index)
+		// clients.splice(index, 1);
+		// console.log(clients.length);
+	  });
+
+	  client.on('geo', function(data) {
+		console.log('geo MESSAGE received from: ' + client.id);
+		currentClient = client.id;
+		//console.log(data);
+		var loc = data.lat + "," + data.long
+		//console.log(loc)
+		gm.reverseGeocode(loc, function(err, data){
+
+		  var city = data.results[0].address_components[2].long_name;
+		  //console.log(city);
+		  //console.log("client within gm : " , client);
+		  //console.log("this within gm : " , this);
+		  var state = data.results[0].address_components[4].long_name;
+		  var loc = city + ", " + state; 
+		  send(JSON.stringify({ "loc": loc, "clientId":  currentClient}));
+		});
+	  });
+    });
+  });
+
+
+});
---
+++ public/javascripts/jasmine/spec/multiUploadSpec.js
@@ -0,0 +1,17 @@
+describe("MultiUploader", function() {
+  var multiuploader;
+  var song;
+
+  beforeEach(function() {
+    player = new MultiUploader();
+    song = new Song();
+  });
+
+
+
+
+
+
+
+
+});
